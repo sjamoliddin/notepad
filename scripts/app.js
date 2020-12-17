8ce583695cc5b5d1e03b118f1d6797f037bdbb02
sjamoliddin <sjamolbek@mail.ru>
2020-12-17 13:55:28 +0500

    update

--- scripts/app.js
+++ scripts/app.js
@@ -1,27 +1,34 @@
 // Selectors
-let form = document.querySelector('.user-name');
+const form = document.querySelector('.user-name');
+const introContainer = document.querySelector('.intro-container');
 
 // Event Listeners
 document.addEventListener('DOMContentLoaded', checkStorage);
 document.querySelector('.nameBtn').addEventListener('click', changeName);
 
 // Functions 
-function changeName(e){
+function changeName(e) {
     // preventing default submittion of the form
     e.preventDefault();
     // changing logo to the user's name    
     let name = document.querySelector('.name').value;
-    document.querySelector('.logo a').textContent = `${name}`;
-    // removing the whole form section    
-    form.remove();   
-    // storing user's name to the storage
-    localStorage.setItem('userName', JSON.stringify(name));    
+    if (!(name === "")) {
+        document.querySelector('.logo a').textContent = `${name}`;
+        // removing the whole form section            
+        form.remove();        
+        introContainer.style.opacity = '1';
+        // storing user's name to the storage
+        localStorage.setItem('userName', JSON.stringify(name));
+    } else {
+        alert('please, enter your name :)');
+    }
 }
 
-function checkStorage(){
-    let name = localStorage.getItem('userName');    
-    if(name){
-        document.querySelector('.logo a').textContent = `${name.split('"')[1]}`;
-        form.remove();   
+function checkStorage() {
+    let name = localStorage.getItem('userName');
+    if (name) {                
+        document.querySelector('.logo a').textContent = `${name.split('"')[1]}`;        
+        form.remove();        
+        introContainer.style.opacity = '1';
     }
 }
