3f58488c67d3bc1e682ac71821215c0f52539fb2
sjamoliddin <sjamolbek@mail.ru>
2020-12-23 19:44:40 +0500

    add delete functionality

--- scripts/posts.js
+++ scripts/posts.js
@@ -2,26 +2,41 @@
 const mainContainer = document.querySelector('main');
 
 // Event Listeners
-document.addEventListener('DOMContentLoaded', showPosts);
+document.addEventListener('DOMContentLoaded', displayPosts);
 
 // Functions
-function showPosts() {
+function displayPosts() {
     // checking if posts are available
     if (localStorage.length > 1) {
         document.querySelector('.intro-content').remove();
+        createPosts();
+        document.querySelectorAll('.btn-delete').forEach(button => {
+            button.addEventListener('click', deletePost);
+        });
     }
+}
 
+function createPosts() {
     let posts = JSON.parse(localStorage.getItem('post'));
 
     for (let i = 0; i < posts.length; i++) {
         // create post container
         const postDiv = document.createElement('div');
-        postDiv.classList.add('post-container');        
+        postDiv.classList.add('post-container');
+        // create title container
+        const titleContianer = document.createElement('div');
+        titleContianer.classList.add('title-container');
         // create post title
         const postTitle = document.createElement('h2');
         postTitle.classList.add('post-title');
         postTitle.innerText = posts[i].title;
-        postDiv.appendChild(postTitle);
+        titleContianer.appendChild(postTitle);
+        // create delete button
+        const btnDelete = document.createElement('button');
+        btnDelete.classList.add('btn-delete');
+        btnDelete.innerHTML = `<i class="fas fa-trash"></i>`
+        titleContianer.appendChild(btnDelete);
+        postDiv.appendChild(titleContianer);
         // create post itsel
         const post = document.createElement('p');
         post.classList.add('post');
@@ -30,4 +45,36 @@ function showPosts() {
         // add postDiv to actual container
         mainContainer.appendChild(postDiv);
     }
+}
+
+function deletePost(e) {
+    // add delete animation
+    const currentPost = e.currentTarget.parentElement.parentElement;
+    currentPost.classList.add('fall');
+
+    // delete the post from storage
+    let posts = checkStorage();
+    const postIndex = e.currentTarget.parentElement.children[0].innerText;    
+    posts.forEach((post) =>{                
+        if(post.title === postIndex){
+            // deleting
+            posts.splice(posts.indexOf(post), 1);
+        }
+    })
+    localStorage.setItem('post', JSON.stringify(posts));
+
+    // removing the post from display
+    currentPost.addEventListener('transitionend', () => {
+        currentPost.remove();
+    });
+}
+
+function checkStorage() {
+    let posts;
+    if (localStorage.getItem('post') === null) {
+        posts = []
+    } else {
+        posts = JSON.parse(localStorage.getItem('post'));
+    }
+    return posts;
 }
